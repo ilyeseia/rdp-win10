b9c93aa3ce0c3e300502eb966381ebc301d72b51
ilyeseia <42816665+ilyeseia@users.noreply.github.com>
2024-11-06 13:16:13 +0100

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -1,4 +1,4 @@
-wget -O ng.sh https://github.com/ilyeseia/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://github.com/ilyeseia/rdp-win10/raw/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 
